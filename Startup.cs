08cc65298512269b2fcd668f47eed90867a41058
juni <yyoona86@gmail.com>
2018-04-20 13:22:27 -0700

    add form page and index page to Views, add wwwroot folder and link to _Layout file, fix Startup.cs, rename .csproj, add foreach to Controllers

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace addressBK
         public void Configure(IApplicationBuilder app)
         {
             app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
